d8e8d95ce06f38962b2a7141d1187d025cfcfbc8
adnan12398 <62546710+adnan12398@users.noreply.github.com>
2023-02-13 14:47:36 +0000

    finished the youtube api search bar

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+/*
+recipe api request, someone else is working on it now
+
 const apiURL = "https://api.spoonacular.com/recipes/"
 const Apikey= "75631c11b2144e099239ea019d07a989"
 var recipeId;
@@ -38,3 +41,43 @@ function getRecipe(){
 
     console.log("this is working")
 }
+"https://www.googleapis.com/youtube/v3/search?part=snippet&key="
+*/
+
+$(document).ready(function(){
+
+    const apiKey = "AIzaSyCkT-nixMdwXNJYm8c3Qd_ScBUzmZ7r0Sk"
+    
+    var video = ""
+    
+    $('form').submit(function(event){
+        event.preventDefault();
+
+        var userInput = $('#user-input').val()
+
+        videoSearch(apiKey, userInput,1);
+
+    })
+
+
+    function videoSearch(apiKey,userInput,max){
+
+        $.ajax({
+            url: "https://www.googleapis.com/youtube/v3/search?key=" + apiKey +"&type=video&part=snippet&maxResults=" + max + "&q=" + userInput  ,
+            method: "GET"
+        }).then(function(response){
+            
+            response.items.forEach(item => {
+                video = `<iframe width="500" height="315" src="http://www.youtube.com/embed/${item.id.videoId}" frameborder = "0" allowfullscreen></iframe>`
+
+                $("#video").append(video);
+
+            });
+            
+        })
+
+    }
+
+})
+
+
