b1b2c6a73406ff247626299656a89f95c0d3bd74
AishaSK <118477467+AishaSK@users.noreply.github.com>
2023-02-15 19:30:52 +0000

    trying to pull origin

---
+++ script.js
@@ -0,0 +1,76 @@
+var search;
+var rID;
+
+// function searchRecipe(){
+// 	const settings = {
+// 		"async": true,
+// 		"crossDomain": true,
+// 		"url": 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch?query='+search,
+// 		"method": "GET",
+// 		"headers": {
+// 			"X-RapidAPI-Key": "020a1e0fa1mshf7a710d6a5d276bp1c379cjsnfe02e9854d05",
+// 			"X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+// 		}
+// 	};
+	
+// 	$.ajax(settings).done(function (response) {
+// 		let id = response.results[0].id;
+// 		rID = id;
+
+// 		console.log(rID);
+// 		searchInstructions();
+// 	});
+// }
+
+// function searchInstructions(){
+// 	const settings = {
+// 		"async": true,
+// 		"crossDomain": true,
+// 		"url": "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/"+"607224"+"/information",
+// 		"method": "GET",
+// 		"headers": {
+// 			"X-RapidAPI-Key": "020a1e0fa1mshf7a710d6a5d276bp1c379cjsnfe02e9854d05",
+// 			"X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+// 		}
+// 	};
+	
+// 	$.ajax(settings).done(function (response) {
+// 		console.log(response.analyzedInstructions[0].steps);
+// 	});
+// }
+
+// function start(){
+	
+// 	searchRecipe();
+
+// }
+
+// start();
+
+
+var searchInput = $("#btnInput");
+
+searchInput.on("click", function(){
+	let input = $("#searchBar").val();
+
+	console.log(input);
+
+	const settings = {
+				"async": true,
+				"crossDomain": true,
+				"url": 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/complexSearch?query='+input,
+				"method": "GET",
+				"headers": {
+					"X-RapidAPI-Key": "020a1e0fa1mshf7a710d6a5d276bp1c379cjsnfe02e9854d05",
+					"X-RapidAPI-Host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com"
+				}
+			};
+			
+			$.ajax(settings).done(function (response) {
+				let id = response.results[0].id;
+		
+				console.log(id);
+				console.log(response);
+			});
+	
+})
